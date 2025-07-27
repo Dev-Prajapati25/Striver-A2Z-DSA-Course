ce89ba8e025b03e678e077d0025a5b1962446399
Dev-Prajapati25 <pdev25214@gmail.com>
2025-07-27 19:35:19 +0530

    Pattern 2

--- Step 1/Patterns/pattern.py
+++ Step 1/Patterns/pattern.py
@@ -22,9 +22,15 @@ def pattern2(n):
             print("* ", end= "")
         print()
 
+def pattern3(n):
+    for i in range(1, n +1):
+        for j in range(i+1):
+            print(j , "", end="")
+        print()
+
 def main():
     n = int(input(">>>"))
-    pattern2(n)
+    pattern3(n)
 
 if __name__ =="__main__":
     main()
